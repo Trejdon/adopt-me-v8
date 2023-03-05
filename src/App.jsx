0fa3d9aa462987a3d62033c4e0f9dc8633fb7f68
Levi Trejo <ltrejo32@gmail.com>
2023-03-05 13:08:29 -0600

    feat(Split): setup code splitting\ and lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+// Removing these from being normally imported in order to code split them for performance
+// import Details from "./Details";
+// import SearchParams from "./SearchParams";
+
+// These are not loaded on the initial page load, but only as required by the app
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,19 +30,30 @@ const App = () => {
       }}
     >
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AdoptedPetContext.Provider value={adoptedPet}>
-            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-              <Link className="text-6xl text-white hover:text-gray-200" to="/">
-                <h1>Adopt Me!</h1>
-              </Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
-          </AdoptedPetContext.Provider>
-        </QueryClientProvider>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">🍥</h2>
+                </div>
+              }
+            >
+              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+                <Link
+                  className="text-6xl text-white hover:text-gray-200"
+                  to="/"
+                >
+                  <h1>Adopt Me!</h1>
+                </Link>
+              </header>
+              <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+            </Suspense>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
       </BrowserRouter>
     </div>
   );
